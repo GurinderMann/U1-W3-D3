931753a51a14bc8da7302cbd295c23f432e1ada0
Gurinder Mann <manngurinder110@gmail.com>
2023-06-07 15:24:46 +0200

    css remaining

---
+++ js/list.js
@@ -0,0 +1,38 @@
+let newItem = document.getElementById('newitem')
+newItem.addEventListener('submit', function(e){
+    e.preventDefault()
+    const addElement = document.getElementById('toDo')
+    const addElementValue = addElement.value 
+    let newListItem = document.createElement('div')
+    newListItem.classList.add("newList")
+    newListItem.innerHTML =   `
+    <li>${addElementValue}</li>
+    <button type="button" id = "delete">  <i class="fal fa-times"></i></button>
+       
+    `
+    const listDiv = document.querySelector('.list')
+
+    listDiv.appendChild(newListItem)
+
+    const deleteButton = document.querySelectorAll('.newList button');
+    deleteButton.forEach(function(button) {
+      button.addEventListener('click', function(e) {
+        let buttonClicked = e.target;
+        let elementRemove = buttonClicked.parentElement;
+        elementRemove.remove();
+      });
+    });
+    
+
+    listDiv.addEventListener('click', function(e) {
+    if (e.target.tagName === 'LI') {
+    e.target.classList.toggle('completed');
+  }
+});
+
+      
+   addElement.value =""
+})
+
+
+
